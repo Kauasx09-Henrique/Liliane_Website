829eef3018323df79ca05e2eb248f78ec3654b78
Kauã Henrique <kauahenriquesx09@gmail.com>
2025-08-19 17:10:44 -0300

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom'; // Importante!
 import Header from './Components/Header';
-import Footer from './components/Footer';
+import Footer from './Components/Footer';
 import './App.css';
 
 function App() {
