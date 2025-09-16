7b2776e605160b8744db09009474c756d74a39e3
Kauã Henrique <kauahenriquesx09@gmail.com>
2025-09-16 12:20:29 -0300

    mudando home img

--- meu-site-dourado/src/App.jsx
+++ meu-site-dourado/src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 
-// REMOVEMOS as importações do react-router-dom
-import Header from './components/Header'; // Renomeei para Header, como fizemos antes
+import { ParallaxProvider } from 'react-scroll-parallax'; // Importe o Provider
+import Header from './components/Header';
 import Home from './pages/Home';
 import SobreMim from './pages/SobreMim';
 import Contato from './pages/Contato';
@@ -12,29 +12,16 @@ import './App.css';
 
 function App() {
   return (
-    // A tag <Router> foi removida
-    <>
+    // Envolva todo o conteúdo com o ParallaxProvider
+    <ParallaxProvider>
       <Header />
       <main>
-        {/* As "páginas" agora são seções com IDs únicos */}
-        <section id="home">
-          <Home />
-        </section>
-
-        <section id="sobre">
-          <SobreMim />
-        </section>
-
-        <section id="testemunhos">
-          <Testemunhos />
-        </section>
-
-        <section id="contato">
-          <Contato />
-        </section>
+        <section id="home"> <Home /> </section>
+        <section id="sobre"> <SobreMim /> </section>
+        <section id="testemunhos"> <Testemunhos /> </section>
+        <section id="contato"> <Contato /> </section>
       </main>
-      {/* Adicione um componente de Footer (Rodapé) aqui se tiver um */}
-    </>
+    </ParallaxProvider>
   );
 }
 
